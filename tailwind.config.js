af196d03dcd0d2dc6c2372c268316832a8d4418b
Siddique <izhan@chingari.io>
2024-08-25 14:09:21 +0530

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        sm: "0.75 rem", //12px
+        base: "0.875", //14px
+        md: "1rem", //16px
+        lg: "1.25rem", //20px
+        xl: "1.5rem", //24px
+        "2xl": "1.75rem", //28px
+        "3xl": "2.25rem", //36px
+      },
+      colors: {
+        gray: {
+          dark: "#C4C4C4",
+          light: "#E5E5E5",
+        },
+        black: {
+          light: "#242527",
+          dark: "#2B2B2B",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
